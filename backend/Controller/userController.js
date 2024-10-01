fc7df6deb78f4423b780966c189fc84e5bc910a8
Faraz shafi <farazpachu777@gmail.com>
2024-10-01 15:31:53 +0530

    created login functionality with jwt token

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,24 +1,50 @@
+import { json } from "express";
 import User from "../Model/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
-
-// @desc    Get user profile
+// @desc    Register a new user
 // @route   POST /api/users/register
-// @access  Private
+// @access  public
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   const userExist = await User.findOne({ email });
-  if(userExist){
+  if (userExist) {
     return res.status(400).json({ message: "User already exists" });
   }
   const user = await User.create({
     name,
     email,
     password,
-    
-  })
-  if(user){
-    res.status(201).json({ message: "User registered successfully" });
-  }else{
-    res.status(400).json({message: "User registration failed" });
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400).json({ message: "User registration failed" });
+  }
+};
+export const login = async (req, res) => {
+  const { password, email } = req.body;
+  const userExists = await User.findOne({ email });
+
+  // Check if the user exists
+  if (!userExists) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const isPasswordMatched = await userExists.matchPassword(password);
+  if (isPasswordMatched) {
+    return res.status(200).json({
+      _id: userExists._id,
+      name: userExists.name,
+      email: userExists.email,
+      token: generateToken(userExists._id),
+    });
+  } else {
+    return res.status(400).json({ message: "Invalid email or password" });
   }
 };
--- backend/Router/userRouter.js
+++ backend/Router/userRouter.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { registerUser } from "../Controller/userController.js"
+import { login, registerUser } from "../Controller/userController.js"
 
 const router = express.Router()
 
 router.post("/register",registerUser)
+router.post("/login",login)
 
 export default router
